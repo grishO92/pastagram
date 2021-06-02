8d1efdbf4dd00587431b5100064b183f28cdb348
grishO92 <grigoryutta@gmail.com>
2021-06-02 13:05:37 +0300

    added more styling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { tile } from './components/tile.js';
 
 const main = document.getElementById('main');
 
-const homePage = () => html` <header class="header">
-  <nav>
-    <form @click=${onSeach}>
-      <input
-        class="seachFld"
-        type="search"
-        placeholder="search for your favorite pasta"
-      />
-      <input class="searchBtn" type="submit" value="Search" />
-    </form>
-  </nav>
-</header>`;
+const homePage = () => html`
+  <header class="header">
+    <a class="logo">PastaGram</a>
+    <nav>
+      <form @submit=${onSeach}>
+        <input
+          class="seachFld"
+          type="search"
+          placeholder="search for your favorite pasta"
+        />
+        <input class="searchBtn" type="submit" value="Search" />
+      </form>
+    </nav>
+  </header>
+  <div class="photo-grid">
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/lamp.jpeg" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/lamp.jpeg" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/lamp.jpeg" />
+    <img class="card" src="static/pasta.png" />
+    <img class="card" src="static/pasta.png" />
+  </div>
+  <footer class="footer">&copy; No copyrights</footer>
+`;
 
 function onSeach(e) {
   e.preventDefault();
